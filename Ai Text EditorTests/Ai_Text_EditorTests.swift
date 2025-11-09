63cf8aeb21a7ebfe5684777d299789f4b8871ecc
Covion Studio <covionstudio@gmail.com>
2025-11-08 17:15:03 -0800

    Initial Commit

---
+++ Ai Text EditorTests/Ai_Text_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ai_Text_EditorTests.swift
+//  Ai Text EditorTests
+//
+//  Created by Covion Studio on 11/8/25.
+//
+
+import Testing
+@testable import Ai_Text_Editor
+
+struct Ai_Text_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
